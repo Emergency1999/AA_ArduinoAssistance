f06ec35365c1831bc79b11764772c5fd6a432028
Emergency1999 <johannes@schmid-company.de>
2021-08-22 14:37:57 +0200

    added debugprinter

--- include/Assistant.h
+++ include/Assistant.h
@@ -6,6 +6,11 @@
 #define STATUS_ETH 0
 #define STATUS_MQTT 1
 
+#define AASSERT(condition, message) \
+    if (!(condition)) printer.assert(__FILE__ + " at " + __LINE__ + ": " + message)
+
+#define AWARN(mesage) printer.warn()
+
 class Assistant {
    public:
     Assistant();
@@ -29,5 +34,17 @@ class Assistant {
     static bool getAssistantKey(uint16_t key);
     static uint16_t getAssistantCount();
 };
+
+class AssistantPrinter {
+   public:
+    virtual void msg(String mesage){};
+
+    virtual void err(String mesage) { msg(mesage); };
+    virtual void msg(String mesage) { msg(mesage); };
+    virtual void assert(String mesage) { msg(mesage); };
+
+} _AssistantPrinter;
+
+AssistantPrinter *printer = &_AssistantPrinter;
 
 #endif
